e07202f44bf922336126198a2ffcd3b4680d381b
Sofia Lammers <sofia.lammers@gmail,com>
2023-03-08 17:49:40 -0500

    this is my second addition to this repository

--- versioncontrol.py
+++ versioncontrol.py
@@ -8,3 +8,16 @@ def encoder(to_encode):
         decoded.append(digit)
     decoded_string = ''.join(str(e) for e in decoded)
     return decoded_string
+game_continue = True
+while game_continue:
+    print("\nMenu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print("")
+    option = int(input("\nPlease enter an option: "))
+    if option == 1:
+        to_encode = (input("Please enter your password to encode: "))
+        decoded = encoder(to_encode)
+        print("Your password has been encoded and stored!")

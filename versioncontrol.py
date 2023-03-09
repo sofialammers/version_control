afd3d199e34e8f6c041af9eb1ac900d052d2eb35
unknown <joshua.park@ufl.edu>
2023-03-08 20:30:21 -0500

    added decoder function

--- versioncontrol.py
+++ versioncontrol.py
@@ -8,6 +8,22 @@ def encoder(to_encode):
         decoded.append(digit)
     decoded_string = ''.join(str(e) for e in decoded)
     return decoded_string
+
+# joshua park
+def decode(decoded):
+    decoded_pwd = ''
+    input_list = list(decoded)
+    for i in range(0, len(input_list)):
+        input_list[i] = int(input_list[i])
+        if input_list[i] > 2:
+            input_list[i] -= 3
+        else:
+            input_list[i] += 7
+    for i in range(0, len(input_list)):
+        decoded_pwd += str(input_list[i])
+    return decoded_pwd
+
+
 game_continue = True
 while game_continue:
     print("\nMenu")
@@ -21,3 +37,6 @@ while game_continue:
         to_encode = (input("Please enter your password to encode: "))
         decoded = encoder(to_encode)
         print("Your password has been encoded and stored!")
+    elif option == 2:
+        decoded_pwd = decode(decoded)
+        print("Your password is:", decoded_pwd)
